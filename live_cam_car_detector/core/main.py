739530986cdd8c9ed5b7fbc3589c8f58959b7764
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2023-10-06 00:01:33 -0500

    Feat limited class box drawing to only selection

--- live_cam_car_detector/core/main.py
+++ live_cam_car_detector/core/main.py
@@ -1,22 +1,30 @@
 from ultralytics import YOLO
 import cv2
-import math 
+import math
+
 # start webcam
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+print(cv2.__version__)
+width = 640
+height = 480
+cam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+cam.set(cv2.CAP_PROP_FPS, 30)
 
 # object classes
-from .constants import classNamesSelection
-classCodes = list(classNamesSelection.keys())
-classNames = list(classNamesSelection.values())
+from .constants import fullClassNamesCodes, classNamesSelection
+
+classCodes = list(fullClassNamesCodes.keys())
+classNames = list(fullClassNamesCodes.values())
+
+#print(classCodes, classNames)
 
 # model
 model = YOLO("yolo-Weights/yolov8n.pt")
 
 while True:
-    success, img = cap.read()
-    results = model.predict(img, stream=True, classes=classCodes)
+    success, img = cam.read()
+    results = model.predict(img, stream=True)
 
     # coordinates
     for r in results:
@@ -25,33 +33,35 @@ while True:
         for box in boxes:
             # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
-
-            # class name            
+            #class code
             cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+            if cls in classNamesSelection:
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
+                # confidence
+                confidence = math.ceil((box.conf[0] * 100)) / 100
+                print("Confidence --->", confidence)
 
-            #cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-            custom_label = f"{classNames[cls]}:{confidence}"
-            cv2.putText(img, custom_label, org, font, fontScale, color, thickness)
+                # class name                
+                print("Class name -->", classNames[cls])
 
-    cv2.imshow('Webcam', img)
-    if cv2.waitKey(1) == ord('q'):
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                # cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                custom_label = f"{classNames[cls]}:{confidence}"
+                cv2.putText(img, custom_label, org, font, fontScale, color, thickness)
+
+    cv2.imshow("Webcam", img)
+    if cv2.waitKey(1) == ord("q"):
         break
 
-cap.release()
+cam.release()
 cv2.destroyAllWindows()
